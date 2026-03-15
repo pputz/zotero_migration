5acae2f7c9dda7e88610ae72ad837d00660f6b6a
Peter Putz <putzpeter@yahoo.com>
2026-03-15 20:19:07 +0100

    Fix regex escaping in script 02a

--- R/02a_fix_bibliographical_references.R
+++ R/02a_fix_bibliographical_references.R
@@ -18,41 +18,41 @@ sources <- read_csv(input_csv, col_types = cols(.default = "c"))
 # ---- HELPERS ------------------------------------------------
 
 na_if_empty <- function(x) {
-  x %>%
-    str_squish() %>%
+  x |>
+    str_squish() |>
     na_if("")
 }
 
 normalize_author <- function(x) {
   x <- na_if_empty(x)
 
-  x %>%
-    str_replace_all("\s*;\s*", "; ") %>%
-    str_replace_all("\s*,\s*", ", ") %>%
-    str_replace_all("\s+", " ") %>%
+  x |>
+    str_replace_all("\\s*;\\s*", "; ") |>
+    str_replace_all("\\s*,\\s*", ", ") |>
+    str_replace_all("\\s+", " ") |>
     str_trim()
 }
 
 normalize_title <- function(x) {
-  x %>%
-    na_if_empty() %>%
-    str_replace_all("[[:space:]]+", " ") %>%
-    str_replace_all("\s+([,.;:])", "\1") %>%
+  x |>
+    na_if_empty() |>
+    str_replace_all("[[:space:]]+", " ") |>
+    str_replace_all("\\s+([,.;:])", "\\1") |>
     str_trim()
 }
 
 normalize_place_publication <- function(x) {
-  x %>%
-    na_if_empty() %>%
-    str_replace_all("\s*:\s*", ": ") %>%
-    str_replace_all("\s*;\s*", "; ") %>%
-    str_replace_all("\s+", " ") %>%
+  x |>
+    na_if_empty() |>
+    str_replace_all("\\s*:\\s*", ": ") |>
+    str_replace_all("\\s*;\\s*", "; ") |>
+    str_replace_all("\\s+", " ") |>
     str_trim()
 }
 
 # ---- TRANSFORM ---------------------------------------------
 
-sources_fixed <- sources %>%
+sources_fixed <- sources |>
   mutate(
     source_id = na_if_empty(source_id),
     title = normalize_title(title),
